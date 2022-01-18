cd7700c2602ee9c4bc05a19287f02e6e69ad03ad
Gustavo Sanchez <gustavo.sanchez@canonical.com>
2022-01-18 15:13:43 -0400

    Add functional tests
    Add link to launchpad bug tracker
    Migrate test bundles to charmhub
    
    func-test-pr: https://github.com/openstack-charmers/zaza-openstack-tests/pull/700
    Change-Id: I1c92d046940c5a61cbd1e082fd34ab7a77ae0a4f

---
+++ tests/tests.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+# Copyright 2019 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Encapsulate cinder-nimblestorage testing."""
+
+from zaza.openstack.charm_tests.cinder_backend.tests import CinderBackendTest
+from os import environ
+
+
+class CinderNimblestorageTest(CinderBackendTest):
+    """Encapsulate cinder-nimblestorage tests."""
+
+    backend_name = 'cinder-nimblestorage'
+
+    expected_config_content = {
+        'cinder-nimblestorage': {
+            'volume_driver':
+            ['cinder.volume.drivers.nimble.NimbleISCSIDriver'],
+            'san_ip': environ['TEST_NIMBLESTORAGE_SAN_IP'],
+            'san_login': environ['TEST_NIMBLESTORAGE_SAN_USERNAME'],
+            'san_password': environ['TEST_NIMBLESTORAGE_SAN_PASSWORD']
+        }}
